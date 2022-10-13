74d455e4db38036ccd1df92182618f9235bb1a17
nikitiser <nikitiser@gmail.com>
2022-10-14 03:00:00 +0600

    open channel

--- lib/src/yandex_search.dart
+++ lib/src/yandex_search.dart
@@ -3,25 +3,20 @@ part of yandex_mapkit;
 /// Main interface for searching.
 class YandexSearch {
   static const String _channelName = 'yandex_mapkit/yandex_search';
-  static const MethodChannel _channel = MethodChannel(_channelName);
+  static const MethodChannel channel = MethodChannel(_channelName);
 
   static int _nextSessionId = 0;
 
   /// Search request for searching a user query near given geometry.
-  static SearchResultWithSession searchByText({
-    required String searchText,
-    required Geometry geometry,
-    required SearchOptions searchOptions
-  }) {
+  static SearchResultWithSession searchByText(
+      {required String searchText, required Geometry geometry, required SearchOptions searchOptions}) {
     final params = <String, dynamic>{
       'sessionId': _nextSessionId++,
       'searchText': searchText,
       'geometry': geometry.toJson(),
       'searchOptions': searchOptions.toJson(),
     };
-    final result = _channel
-      .invokeMethod('searchByText', params)
-      .then((result) => SearchSessionResult._fromJson(result));
+    final result = channel.invokeMethod('searchByText', params).then((result) => SearchSessionResult._fromJson(result));
 
     return SearchResultWithSession._(
       session: SearchSession._(id: params['sessionId']),
@@ -30,20 +25,16 @@ class YandexSearch {
   }
 
   /// Reverse search request (to search objects at the given coordinates)
-  static SearchResultWithSession searchByPoint({
-    required Point point,
-    int? zoom,
-    required SearchOptions searchOptions
-  }) {
+  static SearchResultWithSession searchByPoint(
+      {required Point point, int? zoom, required SearchOptions searchOptions}) {
     final params = <String, dynamic>{
       'sessionId': _nextSessionId++,
       'point': point.toJson(),
       'zoom': zoom,
       'searchOptions': searchOptions.toJson(),
     };
-    final result = _channel
-      .invokeMethod('searchByPoint', params)
-      .then((result) => SearchSessionResult._fromJson(result));
+    final result =
+        channel.invokeMethod('searchByPoint', params).then((result) => SearchSessionResult._fromJson(result));
 
     return SearchResultWithSession._(
       session: SearchSession._(id: params['sessionId']),
